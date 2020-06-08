30deec9bac3505804276546dbdee576915cc502c
neurora <zitonglu1996@gmail.com>
2020-06-09 01:10:03 +0800

    Calculate the Mahalanobis Distance

---
+++ MD_cal.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8
+
+"""
+@File       :   MD_cal.py
+@Author     :   Zitong Lu
+@Contact    :   zitonglu1996@gmail.com
+@License    :   MIT License
+"""
+
+import numpy as np
+from scipy.spatial.distance import mahalanobis
+
+def mahalanobis_distance(p, distr):
+
+    # p: a point
+    # distr : a distribution
+
+    # covariance shape
+    cov = np.cov(distri, rowvar=False)
+
+    # average of the points in distr
+    avg_distri = np.average(distr, axis=0)
+
+    dis = mahalanobis(p, avg_distri, cov)
+
+    return dis
